c59d1bdd7e300a3efa1b1b97f00549d77bea6f90
Nurein1 <nurein90@hotmail.com>
2023-11-14 08:58:59 +0300

    second commit

--- assetgatepass/urls.py
+++ assetgatepass/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 #Url Configuarations
 urlpatterns = [
     path('main/', views.main),
+    path('sgs/', views.sgs),
+    path('security/', views.security),
 ]
--- assetgatepass/views.py
+++ assetgatepass/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def main(request):
     return render(request, 'main.html')
+
+
+def sgs(request):
+    return render(request, 'sgs.html')
+
+
+def security(request):
+    return render(request, 'security.html')
